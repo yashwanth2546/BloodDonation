5b482de058abbcffa335c848d24fa83ff5a58802
alamin1x0 <msasakibalamin@gmail.com>
2022-01-23 17:03:47 +0600

    Readme.md

--- app/src/main/java/com/developeralamin/blood/email/Util.java
+++ app/src/main/java/com/developeralamin/blood/email/Util.java
@@ -2,6 +2,6 @@ package com.developeralamin.blood.email;
 
 public class Util {
     // Add your own email and password over here.ADD YOUR OWN EMAIL AND PASSWORD
-    public  static  final String EMAIL = "dev3sakib@gmail.com";
+    public  static  final String EMAIL = "";
     public static final String PASSWORD = "msa8@Sakib";
 }
