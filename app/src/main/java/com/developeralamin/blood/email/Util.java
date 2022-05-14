0addb1dd601b32b81857e98da7b5024ba6eb0447
yashwanth <katike.yashwanth19@st.niituniversity.in>
2022-05-14 13:47:36 +0530

    initi commit fork

--- app/src/main/java/com/developeralamin/blood/email/Util.java
+++
@@ -1,7 +0,0 @@
-package com.developeralamin.blood.email;
-
-public class Util {
-    // Add your own email and password over here.ADD YOUR OWN EMAIL AND PASSWORD
-    public  static  final String EMAIL = "";
-    public static final String PASSWORD = "";
-}
