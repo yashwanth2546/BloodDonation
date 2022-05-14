a4f4aaabf7b7f64e6ee18d58b91c9c9ec04ff36e
yashwanth <katike.yashwanth19@st.niituniversity.in>
2022-05-14 13:54:13 +0530

    init

---
+++ app/src/main/java/com/example/blood/email/Util.java
@@ -0,0 +1,7 @@
+package com.example.blood.email;
+
+public class Util {
+    // Add your own email and password over here.ADD YOUR OWN EMAIL AND PASSWORD
+    public  static  final String EMAIL = "";
+    public static final String PASSWORD = "";
+}
